9aa2271266f93e1aabce46e6c6594a6c3d0d1f5e
Unknown <theboss07077@gmail.com>
2017-12-03 17:38:59 -0500

    follow cam

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class FollowCam : MonoBehaviour {
 
 	// Use this for initialization
+	public GameObject TruckObj;
 	public Transform Truck;
 	public float smoothTime = 0.3f;
 	public float height = 20;
 	private Vector3 velocity = Vector3.zero;
 
 	void FixedUpdate () {
+		//Vector3 goalRot = new Vector3 
+
 		Vector3 goalPos = Truck.position;
 		goalPos.y = height + Truck.transform.position.y;
 		transform.position = Vector3.SmoothDamp (transform.position, goalPos, ref velocity, smoothTime);
+
+		transform.localEulerAngles =  new Vector3(70, TruckObj.transform.eulerAngles.y + 180, 0);
 	}
 }
