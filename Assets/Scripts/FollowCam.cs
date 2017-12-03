b8c6b822241cb05981c289c32cb8962b9f80845e
Unknown <theboss07077@gmail.com>
2017-12-03 16:27:36 -0500

    follow cam

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform Truck;
+	public float smoothTime = 0.3f;
+	public float height = 20;
+	private Vector3 velocity = Vector3.zero;
+
+	void FixedUpdate () {
+		Vector3 goalPos = Truck.position;
+		goalPos.y = height + Truck.transform.position.y;
+		transform.position = Vector3.SmoothDamp (transform.position, goalPos, ref velocity, smoothTime);
+	}
+}
