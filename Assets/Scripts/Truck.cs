406df61c4ecc16af839acf4ffa1b8b735f97e799
Unknown <theboss07077@gmail.com>
2018-01-11 13:04:08 -0500

    updated truck script

--- Assets/Scripts/Truck.cs
+++ Assets/Scripts/Truck.cs
@@ -15,12 +15,22 @@ public class Truck : MonoBehaviour {
 	public List<AxleInfo> axleInfos; 
 	public float maxMotorTorque;
 	public float maxSteeringAngle;
+	private WheelCollider[] wheels;
+	public float Brakes = 0;
+	public int Bforce = 500;
+	public WheelFrictionCurve frictionCurve;
+	public float slip = 0.8f;
 
+	public void Start(){
+		wheels = GetComponentsInChildren<WheelCollider>();
+
+	}
 	// finds the corresponding visual wheel
 	// correctly applies the transform
 	public void ApplyLocalPositionToVisuals(WheelCollider collider)
 	{
-		if (collider.transform.childCount == 0) {
+		if (collider.transform.childCount == 0) 
+		{
 			return;
 		}
 
@@ -36,20 +46,37 @@ public class Truck : MonoBehaviour {
 
 	public void FixedUpdate()
 	{
+		Debug.Log (Brakes);
 		float motor = maxMotorTorque * Input.GetAxis("Vertical");
 		float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
-
-		foreach (AxleInfo axleInfo in axleInfos) {
-			if (axleInfo.steering) {
+		Brakes = Input.GetAxis ("Jump");
+		foreach (AxleInfo axleInfo in axleInfos)
+		{
+			if (axleInfo.steering) 
+			{
 				axleInfo.leftWheel.steerAngle = steering;
 				axleInfo.rightWheel.steerAngle = steering;
 			}
-			if (axleInfo.motor) {
+			if (axleInfo.motor)
+			{
 				axleInfo.leftWheel.motorTorque = motor;
 				axleInfo.rightWheel.motorTorque = motor;
+
 			}
+				
 			ApplyLocalPositionToVisuals(axleInfo.leftWheel);
 			ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+
+
+
 		}
+		foreach(var Wheel in wheels)
+		{
+			Wheel.brakeTorque = Brakes * Bforce;
+
+		}
+
+
+
 	}
 }
